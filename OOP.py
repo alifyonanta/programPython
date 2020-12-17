dc150f91d97305999db2418cd7ab28a2fe935d4d
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2020-12-17 17:11:51 +0700

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -38,3 +38,134 @@ print(fido.color)
 print(fido.name)
 fido.bark()
 
+class Wolf: 
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def bark(self):
+        print("Grr...")
+
+class Dog(Wolf):
+    def bark(self):
+        print("Gogok")
+
+husky = Dog("Maximal", "buabu")
+husky.bark()
+print(husky.color)
+
+class A:
+    def method(self):
+        print("A method")
+    
+class B(A):
+    def another_method(self):
+        print("B method")
+    
+class C(B):
+    def third_method(self):
+        print("C method")
+    
+c = C()
+c.method()
+c.another_method()
+c.third_method()
+
+class A:
+  def a(self):
+    print(1)
+class B(A):
+  def a(self):
+    print(2)
+	
+class C(B):
+  def c(self):
+    print(3)
+		
+c = C()
+c.a()
+a = A()
+a.a()
+
+class A:
+    def spam(self):
+        print(1)
+
+class B(A):
+    def spam(self):
+        print(2)
+        super().spam()
+
+B().spam()
+
+#Magic Methods
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+first = Vector2D(5, 7)
+second = Vector2D(3, 9)
+result = first + second
+print(result.x)
+print(result.y)
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __truediv__(self, other):
+        line = "=" * len(other.cont)
+        return "\n".join([self.cont, line, other.cont])
+
+spam = SpecialString("spam")
+hello = SpecialString("Hello world!")
+print(spam / hello)
+
+# __lt__ for <
+# __le__ for <=
+# __eq__ for ==
+# __ne__ for !=
+# __gt__ for >
+# __ge__ for >=
+
+# __len__ for len()
+# __getitem__ for indexing
+# __setitem__ for assigning to indexed values
+# __delitem__ for deleting indexed values
+# __iter__ for iteration over objects (e.g., in for loops)
+# __contains__ for in
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __gt__(self, other):
+        for index in range(len(other.cont)+1):
+            result = other.cont[:index] + ">" + self.cont
+            result += ">" + other.cont[index:]
+            print(result)
+
+spam = SpecialString("spam")
+eggs = SpecialString("eggs")
+spam > eggs
+
+import random
+
+class VagueList:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __getitem__(self, index):
+        return self.cont[index + random.randint(-1, 1)]
+
+    def __len__(self):
+        return random.randint(0, len(self.cont)*2)
+
+vague_list = VagueList(["A", "B", "C", "D", "E"])
+print(len(vague_list))
+print(len(vague_list))
+print(vague_list[2])
+print(vague_list[2])

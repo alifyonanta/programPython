6cd187799d56277a509ade76580756ffb25dae97
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2021-10-21 21:37:42 +0700

    Update Data Cleansing.py

--- dqlabs-proa/data-quality/Data Cleansing.py
+++ dqlabs-proa/data-quality/Data Cleansing.py
@@ -15,3 +15,28 @@ print(retail_raw['quantity'].fillna(retail_raw.quantity.mean()))
 # Drop missing value
 print('\nDrop missing value:')
 print(retail_raw['quantity'].dropna())
+
+print(retail_raw['item_price'].fillna(retail_raw['item_price'].mean()))
+
+# Q1, Q3, dan IQR
+Q1 = retail_raw['quantity'].quantile(0.25)
+Q3 = retail_raw['quantity'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Check ukuran (baris dan kolom) sebelum data yang outliers dibuang
+print('Shape awal: ', retail_raw.shape)
+
+# Removing outliers
+retail_raw = retail_raw[~((retail_raw['quantity'] < (Q1 - 1.5 * IQR)) | (retail_raw['quantity'] > (Q3 + 1.5 * IQR)))]
+
+# Check ukuran (baris dan kolom) setelah data yang outliers dibuang
+print('Shape akhir: ', retail_raw.shape)
+
+# Check ukuran (baris dan kolom) sebelum data duplikasi dibuang
+print('Shape awal: ', retail_raw.shape)
+
+# Buang data yang terduplikasi
+retail_raw.drop_duplicates(inplace=True)
+
+# Check ukuran (baris dan kolom) setelah data duplikasi dibuang
+print('Shape akhir: ', retail_raw.shape)

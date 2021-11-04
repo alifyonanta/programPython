836cff8ec83256c8dca829d007d111692dc03ca5
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2021-11-04 23:07:46 +0700

    Create Aggregation & GroupBy.py

---
+++ dqlabs-proa/Data Manipulation with Pandas/Aggregation & GroupBy.py
@@ -0,0 +1,39 @@
+import pandas as pd
+# Load data global_air_quality.csv
+global_air_quality = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv')
+print('Lima data teratas:\n', global_air_quality.head())
+# Melakukan pengecekan terhadap data
+print('Info global_air_quality:\n', global_air_quality.info())
+# Melakukan count tanpa groupby
+print('Count tanpa groupby:\n', global_air_quality.count())
+# Melakukan count dengan groupby 
+gaq_groupby_count = global_air_quality.groupby('source_name').count()
+print('Count dengan groupby (5 data teratas):\n', gaq_groupby_count.head())
+
+# Create variabel pollutant 
+pollutant = gaq[['country','city','pollutant','value']].pivot_table(index=['country','city'],columns='pollutant').fillna(0)
+print('Data pollutant (5 teratas):\n', pollutant.head())
+# [1] Group berdasarkan country dan terapkan aggregasi mean
+pollutant_mean = pollutant.groupby('country').mean()
+print('Rata-rata pollutant (5 teratas):\n', pollutant_mean.head())
+# [2] Group berdasarkan country dan terapkan aggregasi std
+pollutant_std = pollutant.groupby('country').std().fillna(0)
+print('Standar deviasi pollutant (5 teratas):\n', pollutant_std.head())
+
+# [3] Group berdasarkan country dan terapkan aggregasi sum
+pollutant_sum = pollutant.groupby('country').sum()
+print('Total pollutant (5 teratas):\n', pollutant_sum.head())
+# [4] Group berdasarkan country dan terapkan aggregasi nunique
+pollutant_nunique = pollutant.groupby('country').nunique()
+print('Jumlah unique value pollutant (5 teratas):\n', pollutant_nunique.head())
+
+# Group berdasarkan country dan terapkan aggregasi first
+pollutant_first = pollutant.groupby('country').first()
+print('Item pertama pollutant (5 teratas):\n', pollutant_first.head())
+# Group berdasarkan country dan terapkan aggregasi last
+pollutant_last = pollutant.groupby('country').last()
+print('Item terakhir pollutant (5 teratas):\n', pollutant_last.head())
+
+# Group berdasarkan country dan terapkan aggregasi: min, median, mean, max
+multiagg = pollutant.groupby('country').agg(['min','median','mean','max'])
+print('Multiple aggregations (5 teratas):\n', multiagg.head())

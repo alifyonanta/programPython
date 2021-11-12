0270fb30da6ddcd32c4877e24c023a879655fa5b
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2021-11-12 15:46:18 +0700

    Update datacleansing.py

--- dqlabs-proa/Data Science in Telco/datacleansing.py
+++ dqlabs-proa/Data Science in Telco/datacleansing.py
@@ -1,5 +1,8 @@
 #import library
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 pd.options.display.max_columns = 50
 #import dataset
 df_load = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqlab_telco.csv')
@@ -13,3 +16,45 @@ print(df_load.customerID.nunique())
 df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
 df_load = (df_load[df_load['valid_id']==True]).drop('valid_id',axis=1)
 print('Hasil jumlah ID Customer yang terfilter adalah',df_load['customerID'].count())
+
+# Drop Duplicate Rows
+df_load.drop_duplicates()
+# Drop duplicate ID sorted by Periode
+df_load = df_load.sort_values('UpdatedAt',ascending=False).drop_duplicates(['customerID'])
+print('Hasil jumlah ID Customer yang sudah dihilangkan duplikasinya (distinct) adalah',df_load['customerID'].count())
+
+print('Total missing values data dari kolom Churn',df_load['Churn'].isnull().sum())
+# Dropping all Rows with spesific column (churn)
+df_load.dropna(subset=['Churn'],inplace=True)
+print('Total Rows dan kolom Data setelah dihapus data Missing Values adalah',df_load.shape)
+
+print('Status Missing Values :',df_load.isnull().values.any())
+print('\nJumlah Missing Values masing-masing kolom, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+
+#handling missing values Tenure fill with 11
+df_load['tenure'].fillna(11,inplace=True)
+
+#Loop
+#Handling missing values num vars (except Tenure)
+for col_name in list(['MonthlyCharges','TotalCharges']):
+    #write your command here
+	median=df_load[col_name].median()
+	df_load[col_name].fillna(median,inplace=True)
+print('\nJumlah Missing Values setelah di imputer datanya, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+
+print('\nPersebaran data sebelum ditangani Outlier: ')
+print(df_load[['tenure','MonthlyCharges','TotalCharges']].describe())
+
+# Creating Box Plot
+
+plt.figure()
+sns.boxplot(x=df_load['tenure'])
+plt.show()
+plt.figure()
+sns.boxplot(x=df_load['MonthlyCharges'])
+plt.show()
+plt.figure()
+sns.boxplot(x=df_load['TotalCharges'])
+plt.show()

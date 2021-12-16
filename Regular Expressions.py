50b6c6cbbbb735b41ac4e057052ad7b092f58a24
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2021-12-16 14:56:13 +0700

    Update Regular Expressions.py

--- Regular Expressions.py
+++ Regular Expressions.py
@@ -183,3 +183,51 @@ match = re.match(pattern, "griy")
 if match:
      print ("Match 3")    
 
+pattern = r"(.+) \1"
+
+match = re.match(pattern, "word word")
+if match:
+    print ("Match 1")
+
+match = re.match(pattern, "?! ?!")
+if match:
+    print ("Match 2")    
+
+match = re.match(pattern, "abc cde")
+if match:
+    print ("Match 3")
+    
+pattern = r"(\D+\d)"
+
+match = re.match(pattern, "Hi 999!")
+if match:
+    print("Match 1")
+
+match = re.match(pattern, "1, 23, 456!")
+if match:
+    print("Match 2")
+
+match = re.match(pattern, " ! $?")
+if match:
+    print("Match 3")     
+
+pattern = r"\b(cat)\b"
+
+match = re.search(pattern, "The cat sat!")
+if match:
+    print ("Match 1")
+
+match = re.search(pattern, "We s>cat<tered?")
+if match:
+    print ("Match 2")
+
+match = re.search(pattern, "We scattered.")
+if match:
+    print ("Match 3")
+    
+pattern = r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)"
+str = "Please contact info@gmail.com for assistance"
+
+match = re.search(pattern, str)
+if match:
+    print(match.group())

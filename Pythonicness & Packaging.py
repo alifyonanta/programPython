edcaa5893b64047d9a6949e8736ce1a7500bfcc1
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2021-12-17 17:03:08 +0700

    Update Pythonicness & Packaging.py

--- Pythonicness & Packaging.py
+++ Pythonicness & Packaging.py
@@ -76,3 +76,13 @@ if __name__=="__main__":
     print("This is a script")
     
     
+def concatenate(*args):
+    result = None
+    for i in args:
+        if result == None:
+            result = i
+        else:
+            result += "-" + i
+    return result
+
+print(concatenate("I", "love", "Python", "!"))
